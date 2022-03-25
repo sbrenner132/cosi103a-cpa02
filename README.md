d04bbaf50a1098e2b70961a29f20d01bd5a65a8c
Samuel Brenner <sambrenner132@gmail.com>
2022-03-25 09:28:20 -0400

    finishing up testing with nicole

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-# CS103a Spring 22
-
 # PA02: tracker.py and the Transaction class
 
-This git repository is for sharing code developed in the course lessons.
-Each Lesson will be a different branch with the name L??.
-Not all lessons have branches.
+This application is a CLI that allows a user to interface with a SQLite3 database through Python. 
 
+The `Transaction` class is ORM, and has various CRUD operations, as well as contextual summarizations, for the `transactions` table in `tracker.db`.
 
+The `tracker.py` script has been updated to include this new functionality, as well as a new print statement to accomodate for the new table.
 
-
-
+Transcripts for proof of concept for this assignment can be found in `./transcripts`.
--- pa02/test_transaction.py
+++ pa02/test_transaction.py
@@ -11,35 +11,69 @@ def dbfile(tmpdir):
     ''' create a database file in a temporary file system '''
     return tmpdir.join('test_tracker1.db')
 
+
 @pytest.fixture
 def empty_db(dbfile):
     ''' create an empty database '''
     db = Transaction(dbfile)
     yield db
 
+
 @pytest.fixture
 def small_db(empty_db):
-    cat1 = {'row_id':1,'item_num':12, 'amount': 13, 'category': 'parking', 'date':'06-06-2001', 'description': 'the parking ticket actually'}
-    print(cat1)
+    cat1 = {'item_num': 12, 'amount': 13, 'category': 'parking',
+            'date': '06-06-2001', 'description': 'the parking ticket actually'}
+    cat2 = {'item_num': 10, 'amount': 1, 'category': 'parking',
+            'date': '05-05-2001', 'description': 'the parking ticket actually, why????'}
 
-    id1=empty_db.add(cat1)
+    id1 = empty_db.add(cat1)
+    id2 = empty_db.add(cat2)
 
     yield empty_db
 
     empty_db.delete(id1)
+    empty_db.delete(id2)
+
 
 @pytest.mark.summarize_by_date
 def test_summarize_by_date(small_db):
-    small_db.summarize_by_date('06-06-2111')
+    expected = {'rowid': 1, 'item_num': 12, 'amount': 13, 'category': 'parking',
+                'date': '06-06-2001', 'description': 'the parking ticket actually'}
+    actual = small_db.summarize_by_date(
+        '06-06-2001')[0]  # get the first item in the list
+    assert len(expected) == len(actual)
+    assert expected == actual
+
 
 @pytest.mark.summarize_by_month
 def test_summarize__by_month(small_db):
-    small_db.summarize_by_month('06')
+    expected = [{'rowid': 1, 'item_num': 12, 'amount': 13, 'category': 'parking',
+                 'date': '06-06-2001', 'description': 'the parking ticket actually'}]
+    actual = small_db.summarize_by_month('06')
+    assert len(expected) == len(actual)
+    assert len(expected[0]) == len(actual[0])
+    assert expected[0] == actual[0]
+
 
 @pytest.mark.summarize_by_year
 def test_summarize_by_year(small_db):
-    small_db.summarize_by_year('2111')
+    expected = [{'rowid': 1, 'item_num': 12, 'amount': 13, 'category': 'parking', 'date': '06-06-2001', 'description': 'the parking ticket actually'},
+                {'rowid': 2, 'item_num': 10, 'amount': 1, 'category': 'parking', 'date': '05-05-2001', 'description': 'the parking ticket actually, why????'}]
+    actual = small_db.summarize_by_year('2001')
+    assert len(expected) == len(actual)
+    assert len(expected[0]) == len(actual[0])
+    assert len(expected[1]) == len(actual[1])
+    assert expected[0] == actual[0]
+    assert expected[1] == actual[1]
+
 
 @pytest.mark.summarize_by_category
 def test_summarize_by_category(small_db):
-    small_db.summarize_by_category('parking')
+    expected = [{'rowid': 1, 'item_num': 12, 'amount': 13, 'category': 'parking', 'date': '06-06-2001', 'description': 'the parking ticket actually'},
+                {'rowid': 2, 'item_num': 10, 'amount': 1, 'category': 'parking', 'date': '05-05-2001', 'description': 'the parking ticket actually, why????'}]
+    actual = small_db.summarize_by_category('parking')
+    assert len(expected) == len(actual)
+    assert len(expected[0]) == len(actual[0])
+    assert len(expected[1]) == len(actual[1])
+    assert expected[0] == actual[0]
+    assert expected[1] == actual[1]
